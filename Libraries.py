ba5fd867508c85d3ec12d00fa0a2c1216598219d
YousafShakoor <f2021266138@umt.edu.pk>
2024-06-29 19:41:39 +0500

    Create Libraries.py

---
+++ Libraries.py
@@ -0,0 +1,17 @@
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn import svm
+from sklearn.metrics import accuracy_score
+
+# pip install numpy
+# pip install pandas
+
+
+# NumPy: Provides support for arrays and matrices.
+# pandas: For data manipulation and analysis.
+# StandardScaler: Standardizes features by removing the mean and scaling to unit variance.
+# train_test_split: Splits the dataset into training and testing sets.
+# svm: Support Vector Machines for classification and regression tasks.
+# accuracy_score: Calculates the accuracy of the model.
